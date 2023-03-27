5b2b1b14ff01d5b580f4a8f8adc7bf9bcd8a2cf9
Feru <f1nandocor@gmail.com>
2023-03-27 12:04:50 -0600

    Update starship conf

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,6 +1,3 @@
-# Languages
-export GOPATH="$HOME/.go"
-
 export NVM_DIR="$HOME/.config/nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
@@ -13,10 +10,11 @@ export path=(
     "$HOME/bin"
     "$FERU_PATH/bin"
     "$DOTFILES_PATH/bin"
-    "$GOPATH/bin"
+    "$HOME/go/bin"
     "$HOME/.cargo/bin"
     "/opt/homebrew/bin"
     "/usr/local/bin"
+    "/usr/local/go/bin"
     "/bin"
     "/usr/bin"
     "/usr/sbin"
