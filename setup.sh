7b6cef7d27ddd5f1dadcc55784a66ce6226e0b29
Feru <f1nandocor@gmail.com>
2025-12-19 12:07:52 -0600

    Adding setup.sh steps

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 export DOTFILES_PATH="$HOME/dotfiles"
 
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#Install stow
+brew install stow
+
+# Initialize .config 
 stow .
 
+# Create symlinks
 ln -sf $DOTFILES_PATH/git/.gitconfig ~/.gitconfig
 ln -sf $DOTFILES_PATH/git/.gitignore_global ~/.gitignore_global
 ln -sf $DOTFILES_PATH/zshrc/.zshrc ~/.zshrc
