7e92deb6a5e89b32d3263a5edcbd29b21773fe72
Fernando <f1nandocor@gmail.com>
2025-12-19 12:22:02 -0600

    Add aerospace config

--- setup.sh
+++ setup.sh
@@ -15,3 +15,7 @@ ln -sf $DOTFILES_PATH/git/.gitconfig ~/.gitconfig
 ln -sf $DOTFILES_PATH/git/.gitignore_global ~/.gitignore_global
 ln -sf $DOTFILES_PATH/zshrc/.zshrc ~/.zshrc
 ln -sf $DOTFILES_PATH/zshrc/.zprofile ~/.zprofile
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ln -sf $DOTFILES_PATH/aerospace/.aerospace.toml ~/.aerospace.toml
+fi
