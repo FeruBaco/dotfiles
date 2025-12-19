b82b19a95fa3f068390fae7f6483317a99c336a1
Fernando <f1nandocor@gmail.com>
2025-12-19 13:10:07 -0600

    Update wezterm options

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,40 +1,32 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 return {
 	color_scheme = 'Dracula (Official)',
 	enable_tab_bar = true,
 	font_size = 14.0,
 	font = wezterm.font('JetBrains Mono'),
-	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
-
- -- window_background_gradient = {
- --   colors = {
- --     '#0f0c29',
- --   '#302b63',
- --   '#24243e',
- --   }
- -- },
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation =  0.5,
-	-- },
-	-- window_background_opacity = 0.92,
 	window_background_opacity = 0.95,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
 	window_decorations = 'RESIZE',
 	keys = {
-		-- {
-		-- 	key = 'f',
-		-- 	mods = 'ctrl',
-		-- 	action = wezterm.action.togglefullscreen,
-		-- },
 		{
 			key = 'k',
 			mods = 'SUPER',
 			action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+		},
+		{
+			key = 'LeftArrow',
+			mods = 'OPT',
+			action = act.SendKey {
+				key = 'b',
+				mods = 'ALT',
+			},
+		},
+		{
+			key = 'RightArrow',
+			mods = 'OPT',
+			action = act.SendKey { key = 'f', mods = 'ALT' },
 		},
 	},
 	mouse_bindings = {
